b09fe8b6434b70cd810848a66b13bab2274c10ad
pearcej <jan_pearce@berea.edu>
2019-01-17 15:36:19 -0500

    Added files to repo

---
+++ use-functions/use-functions.cpp
@@ -0,0 +1,31 @@
+/* Using functions
+by <your name>*/
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main( ){
+
+string s;
+
+cout << "\n Hello Giorgi!!\n" << endl;
+
+
+int count = 0;
+char endit;
+
+
+
+
+
+
+
+
+cin >> endit;
+
+return 0;
+}
+
